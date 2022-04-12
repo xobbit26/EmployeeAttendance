2d993b7283d693c4dcd3034c0b19310d732d7a90
Stanislav Naravetskas <stanislav.naravetskas@arcadia.spb.ru>
2022-04-12 12:41:14 +0300

    added required constraint to several fields in Employee Entity and clean up a bit

--- EmployeeAttendance/Data/DataContext.cs
+++ EmployeeAttendance/Data/DataContext.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeAttendance.Data.Entities;
+using EmployeeAttendance.Data.EntityTypeConfigurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeAttendance.Data;
@@ -8,16 +9,13 @@ public class DataContext : DbContext
     public DbSet<Employee> Employees { get; set; } = null!;
     public DbSet<Entities.EmployeeAttendance> EmployeeAttendance { get; set; } = null!;
 
+
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //Employee Settings
-        modelBuilder.Entity<Employee>()
-            .HasCheckConstraint("age", "age > 0 and age < 120", c => c.HasName("ck_employee_age"));
-
-        modelBuilder.Entity<Employee>().Property(nameof(Employee.Name)).HasMaxLength(50);
-        modelBuilder.Entity<Employee>().Property(nameof(Employee.Surname)).HasMaxLength(50);
-        modelBuilder.Entity<Employee>().Property(nameof(Employee.Position)).HasMaxLength(100);
+        new EmployeeEntityTypeConfiguration()
+            .Configure(modelBuilder.Entity<Employee>());
     }
 }
---
+++ EmployeeAttendance/Data/EntityTypeConfigurations/EmployeeEntityTypeConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using EmployeeAttendance.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmployeeAttendance.Data.EntityTypeConfigurations
+{
+    public class EmployeeEntityTypeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(nameof(Employee.Name))
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(nameof(Employee.Surname))
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(nameof(Employee.Birthday))
+                .IsRequired();
+
+            builder.Property(nameof(Employee.HiringDate))
+                .IsRequired();
+
+            builder.Property(nameof(Employee.Position))
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(nameof(Employee.Department))
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(nameof(Employee.IsActive))
+                .HasDefaultValue(false);
+
+            builder
+                .HasCheckConstraint("age", "age > 0 and age < 120", c => c.HasName("ck_employee_age"));
+        }
+    }
+}
